bb9a0f7e14eebd61779ea5183b656bfbf77b6f2e
Gautier <gautier@escape.tech>
2023-03-04 23:42:08 -0800

    style: added issue comment

--- packages/app/vite.config.js
+++ packages/app/vite.config.js
@@ -3,6 +3,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  // plugin-node-resolve is required to bundle chalk,
+  // see https://github.com/vitejs/vite/issues/7385
   plugins: [nodeResolve(), sveltekit()],
   server: {
     headers: {
