e0149644e8ad3c51e579d6737c0a7c0609dc5dca
Gautier <gautier@escape.tech>
2023-03-07 00:26:08 -0800

    feat: split overlay

--- packages/app/src/lib/editor/Pane.svelte
+++ packages/app/src/lib/editor/Pane.svelte
@@ -1,13 +1,17 @@
+<script lang="ts" context="module">
+  /** Currently dragged tab. */
+  let dragged = writable<PaneChild | undefined>();
+</script>
+
 <script lang="ts">
   import { flip } from 'svelte/animate';
+  import { writable } from 'svelte/store';
+  import { fade } from 'svelte/transition';
   import { paneComponents, type PaneChild } from './files.js';
   import Icon from './icons/Icon.svelte';
 
   export let children: PaneChild[];
   export let selected: PaneChild | undefined = undefined;
-
-  /** Currently dragged tab. */
-  let dragged: PaneChild | undefined = undefined;
 
   /** Maps elements of `children` to their matching tab element. */
   let tabsMap = new Map<PaneChild, HTMLElement>();
@@ -22,6 +26,9 @@
    *   when `children` is reordered
    */
   let childrenSet = new Set(children);
+
+  /** Splitting direction */
+  let split: 'left' | 'right' | 'up' | 'down' | undefined;
 
   // Tell svelte that updates to `selected` also affect `children`
   $: children = (selected, children);
@@ -54,10 +61,12 @@
   };
 
   /** Updates the tab order in realtime. */
-  const drag = function (this: HTMLElement, event: DragEvent) {
-    if (!dragged) return;
+  const dragTab = function (this: HTMLElement, event: DragEvent) {
+    if (!$dragged) return;
     /** Horizontal coordinate relative to the tabs container. */
-    const x = event.pageX + this.scrollLeft - this.offsetLeft;
+    const rect = this.getBoundingClientRect();
+    console.log(rect);
+    const x = event.pageX - rect.left + this.scrollLeft;
     // const x = event.layer;
     /** Insert the dragged tab after this index. */
     let placeAfterIndex: number | undefined;
@@ -73,16 +82,29 @@
     const reordered = [];
     // If the insertion point has been found before the first tab,
     // `placeAfterIndex` is undefined and the dragged tab is inserted first
-    if (placeAfterIndex === undefined) reordered.push(dragged);
+    if (placeAfterIndex === undefined) reordered.push($dragged);
     for (const [i, child] of children.entries()) {
       // Skip the dragged tab
-      if (child !== dragged) reordered.push(child);
+      if (child !== $dragged) reordered.push(child);
       // Insert the dragged tab after the insertion point
-      if (i === placeAfterIndex) reordered.push(dragged);
+      if (i === placeAfterIndex) reordered.push($dragged);
     }
     // Only update `children` if the order has changed to avoid visual glitches
     if (reordered.some((child, i) => child !== children[i]))
       children = reordered;
+  };
+
+  const dragSplit = function (this: HTMLElement, event: DragEvent) {
+    if (!$dragged) return;
+    const rect = this.getBoundingClientRect();
+    const x = (event.pageX - rect.left + this.scrollLeft) / rect.width - 0.5;
+    const y = (event.pageY - rect.top + this.scrollTop) / rect.height - 0.5;
+    split = 'up';
+    if (Math.abs(x) > Math.abs(y))
+      if (x > 0) split = 'right';
+      else split = 'left';
+    else if (y > 0) split = 'down';
+    this.dataset.split = split;
   };
 </script>
 
@@ -90,7 +112,7 @@
   <div
     class="scroll"
     on:dragenter|preventDefault
-    on:dragover|preventDefault={drag}
+    on:dragover|preventDefault={dragTab}
   >
     <div class="tabs">
       {#each children as child (child)}
@@ -104,7 +126,10 @@
             event.dataTransfer.effectAllowed = 'all';
             event.dataTransfer.dropEffect = 'move';
             selected = child;
-            dragged = child;
+            $dragged = child;
+          }}
+          on:dragend={() => {
+            $dragged = undefined;
           }}
           use:map={child}
           animate:flip={{ duration: 200 }}
@@ -126,21 +151,34 @@
     </div>
   </div>
 
-  {#each [...childrenSet] as child (child)}
-    <div class="contents" hidden={selected !== child}>
-      <svelte:component
-        this={paneComponents[child.type]}
-        bind:context={child.context}
-        on:cmd
-        on:dirty={() => {
-          child.dirty = true;
-        }}
-      />
-    </div>
-  {/each}
+  <div class="wrapper">
+    {#if $dragged}
+      <div
+        class="drag-overlay"
+        on:dragenter|preventDefault
+        on:dragover|preventDefault={dragSplit}
+        transition:fade={{ duration: 100 }}
+      >
+        <div data-split={split} />
+      </div>
+    {/if}
 
-  <div class="contents" hidden={Boolean(selected)}>
-    <span class="empty">📔</span>
+    {#each [...childrenSet] as child (child)}
+      <div class="contents" hidden={selected !== child}>
+        <svelte:component
+          this={paneComponents[child.type]}
+          bind:context={child.context}
+          on:cmd
+          on:dirty={() => {
+            child.dirty = true;
+          }}
+        />
+      </div>
+    {/each}
+
+    <div class="contents" hidden={Boolean(selected)}>
+      <span class="empty">📔</span>
+    </div>
   </div>
 </div>
 
@@ -197,7 +235,43 @@
     }
   }
 
+  .wrapper {
+    position: relative;
+    overflow-y: auto;
+  }
+
+  .drag-overlay {
+    position: absolute;
+    inset: 0;
+    z-index: 1;
+    overflow: hidden;
+
+    [data-split] {
+      position: absolute;
+      inset: 0;
+      background-color: var(--hovered);
+      transition: transform 0.1s;
+    }
+
+    [data-split='up'] {
+      transform: scaleY(0.5) translateY(-50%);
+    }
+
+    [data-split='down'] {
+      transform: scaleY(0.5) translateY(50%);
+    }
+
+    [data-split='left'] {
+      transform: scaleX(0.5) translateX(-50%);
+    }
+
+    [data-split='right'] {
+      transform: scaleX(0.5) translateX(50%);
+    }
+  }
+
   .contents {
+    height: 100%;
     overflow: auto;
     background: var(--focused);
   }
