3da977c9ced3cc5b041048ef33530bd4da2e39b7
Gautier <gautier@escape.tech>
2023-08-30 15:10:57 +0200

    fix: made progress bar work on chrome

--- packages/app/src/routes/Progress.svelte
+++ packages/app/src/routes/Progress.svelte
@@ -8,25 +8,34 @@
   $: $progress = done;
 </script>
 
-<div>
+<div class="wrapper">
   <span>{Math.ceil($progress)}/{total}</span>
-  <meter value={$progress} max={total} />
+  <div class="progress">
+    <div style="width: {100 - ($progress / total) * 100}%" />
+  </div>
   <span class="cup" class:complete={done >= total}> 🏆 </span>
 </div>
 
 <style lang="scss">
-  div {
+  .wrapper {
     display: flex;
     gap: 1rem;
     align-items: center;
   }
 
-  meter {
+  .progress {
+    display: flex;
     flex: 1;
+    justify-content: end;
     height: 1em;
-    appearance: none;
-    border: none;
+    overflow: hidden;
+    background: linear-gradient(to right, #5b9b78, #52cd89);
     border-radius: 0.25rem;
+
+    > div {
+      height: 100%;
+      background: #f2f2f2;
+    }
   }
 
   .cup {
@@ -37,13 +46,14 @@
     opacity: 0.5;
     transition: 0.2s all ease-out;
 
-    div:hover & {
+    .wrapper:hover & {
       color: #0004;
       opacity: 1;
       transform: scale(1.1);
     }
 
-    &.complete {
+    &.complete,
+    .wrapper:hover &.complete {
       color: inherit;
       opacity: 1;
     }
