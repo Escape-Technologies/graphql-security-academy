eb9f34d8cf116605d31ab7ed5ba522cf0357ca6c
Gautier <gautier@escape.tech>
2023-08-30 16:55:12 +0200

    feat: added header links

--- packages/app/src/routes/Header.svelte
+++ packages/app/src/routes/Header.svelte
@@ -1,13 +1,20 @@
 <script lang="ts">
   import logo from '$assets/logo.svg?url';
+  import RiShareBoxFill from '~icons/ri/share-box-fill';
 </script>
 
 <div class="page-header">
   <header>
-    <img src={logo} alt="" />
-    <h1>GraphQL Security Academy</h1>
-    <span class="beta">Beta</span>
-    <div style="flex: 1" />
+    <div class="title">
+      <img src={logo} alt="" />
+      <h1>GraphQL Security Academy</h1>
+      <span class="beta">Beta</span>
+    </div>
+    <div class="links">
+      <a href="https//escape.tech">Website <RiShareBoxFill /></a>
+      <a href="https//escape.tech/company/">About Escape <RiShareBoxFill /></a>
+      <a href="https//escape.tech/blog/">Blog <RiShareBoxFill /></a>
+    </div>
   </header>
 </div>
 
@@ -17,24 +24,39 @@
     flex-wrap: wrap;
     gap: 0.5rem;
     align-items: center;
-    justify-content: center;
-    max-width: 50rem;
-    padding-inline: 1rem;
+    justify-content: space-between;
+    padding-inline: 0.5rem;
     margin: 0 auto;
-
-    > * {
-      text-align: center;
-    }
   }
 
   h1 {
     margin: 0;
   }
 
+  a {
+    text-decoration: none;
+
+    > :global(svg) {
+      width: 1em;
+    }
+  }
+
   .page-header {
     padding: 1rem;
     background: var(--main);
     box-shadow: 0 0 0.5rem var(--dark);
+  }
+
+  .title {
+    display: flex;
+    gap: 0.5rem;
+    align-items: center;
+  }
+
+  .links {
+    display: flex;
+    gap: 1rem;
+    align-items: center;
   }
 
   .beta {
